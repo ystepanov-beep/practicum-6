445cae42746ecbe4d5149beb06183e125c05d253
ystepanov-beep <y.stepanov@g.nsu.ru>
2025-12-02 10:55:53 +0700

    Delete python проджектс directory

--- python проджектс/zadanie 2.py
+++
@@ -1,6 +0,0 @@
-A, B = map(int,input().split("x"))
-C, D, E = map(int,input().split("x"))
-if A >= C or A >= D or A >= E and B >= C or B >= D or B >= E:
-    print("да")
-else:
-    print("нет")
--- python проджектс/zadanie 7.py
+++
@@ -1,8 +0,0 @@
-k = int(input())
-found = False
-for five in range(k // 5 + 1):
-    for seven in range(k // 7 + 1):
-        if five * 5 + seven * 7 == k:
-            found = True
-            break
-print("да" if found else "нет")
