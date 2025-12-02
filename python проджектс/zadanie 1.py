35db6d51cd2cc2e57a614fcd706f93f207a62af5
ystepanov-beep <y.stepanov@g.nsu.ru>
2025-12-02 11:16:35 +0700

    Delete python проджектс directory

--- python проджектс/zadanie 1.py
+++
@@ -1,5 +0,0 @@
-length, width = map(int,input().split("x"))
-if 0 <= (length**2 + width**2)**0.5 <= 13:
-    print("да")
-else:
-    print("нет")
--- python проджектс/zadanie 3.py
+++
@@ -1,6 +0,0 @@
-N, M = map(int,input().split("x"))
-street = int(input())
-if street % N == 0 or street % M == 0 and street < N * M:
-    print("успешно")
-else:
-    print("неосуществимо")
